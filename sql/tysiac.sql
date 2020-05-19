e49ae0ea0846baa84e9a6e13a17d0ed03b2f3b26
AndyWolfNo1 <marcinjanosz8@gmail.com>
2020-05-19 13:02:13 +0200

    update sql

--- sql/tysiac.sql
+++ sql/tysiac.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 17 Maj 2020, 21:33
--- Wersja serwera: 10.1.38-MariaDB
--- Wersja PHP: 7.3.2
+-- Czas generowania: 19 Maj 2020, 13:01
+-- Wersja serwera: 10.4.8-MariaDB
+-- Wersja PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,7 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `gracze` (
   `ID` int(11) NOT NULL,
-  `Name` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `login` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `passwd` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `name` text COLLATE utf8mb4_polish_ci NOT NULL,
   `points` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
@@ -38,11 +40,62 @@ CREATE TABLE `gracze` (
 -- Zrzut danych tabeli `gracze`
 --
 
-INSERT INTO `gracze` (`ID`, `Name`, `points`) VALUES
-(1, 'Buczo', 120),
-(2, 'Seba', 50),
-(3, 'Marcin', 130),
-(4, 'Polmos', 350);
+INSERT INTO `gracze` (`ID`, `login`, `passwd`, `name`, `points`) VALUES
+(1, 'Lasza', 'd4cb0ebeee4fa1c62e27898818eeaa9f', 'Marcin', 0),
+(2, 'Buczo', 'd2e5ff6d027b9f674391b2a402adb363', 'Tomek', 0),
+(3, 'Polmos', 'd09f49dc98df20ae259d6ecd8a61c4f8', 'Tomek', 0),
+(4, 'Sebix', 'd3e62c6c5e96bdee6f23fdcaa2505527', 'Sebastnian', 0),
+(5, 'Prezes', '5a1a31b0ae38f1e6ed3157931e5609cb', 'pioter', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tables`
+--
+
+CREATE TABLE `tables` (
+  `ID` int(11) NOT NULL,
+  `author` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `stan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `tables`
+--
+
+INSERT INTO `tables` (`ID`, `author`, `stan`) VALUES
+(1, 'Lasza', 0),
+(11, 'Buczo', 0),
+(12, 'Lasza', 0),
+(13, 'Polmos', 0),
+(14, 'Sebix', 0),
+(15, 'Sebix', 0),
+(16, 'Prezes', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `t_p`
+--
+
+CREATE TABLE `t_p` (
+  `ID` int(11) NOT NULL,
+  `name` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `chair` int(11) NOT NULL,
+  `table_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `t_p`
+--
+
+INSERT INTO `t_p` (`ID`, `name`, `chair`, `table_ID`) VALUES
+(1, 'buczo', 2, 11),
+(2, 'lasza', 1, 11),
+(3, 'lasza', 1, 1),
+(4, 'polmos', 4, 11),
+(5, 'sebix', 3, 11),
+(6, 'sebix', 4, 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -55,6 +108,18 @@ ALTER TABLE `gracze`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indeksy dla tabeli `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `t_p`
+--
+ALTER TABLE `t_p`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +127,19 @@ ALTER TABLE `gracze`
 -- AUTO_INCREMENT dla tabeli `gracze`
 --
 ALTER TABLE `gracze`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT dla tabeli `tables`
+--
+ALTER TABLE `tables`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT dla tabeli `t_p`
+--
+ALTER TABLE `t_p`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
